84a509d551c69d07874d75de26cfc4d8dc111010
Hoseintyi <hoseint629@gmail.com>
2023-11-19 23:02:55 +0330

    this part created function for choose e random square

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-alert("hello");
+
+var buttonsColors=["red" , "blue" , "green" , "yellow"];
+var gamePattern = [];
+
+function nextSquence(){
+    let n = Math.random();
+    n= n*4;
+    n=Math.floor(n);
+    var randomChosenColor= buttonsColors[n]; 
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+}
+
