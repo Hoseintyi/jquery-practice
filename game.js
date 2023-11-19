9a6bc36ad9169f98619d360694394e532a0b66aa
Hoseintyi <hoseint629@gmail.com>
2023-11-19 23:17:48 +0330

    user can click on buttons

--- game.js
+++ game.js
@@ -1,15 +1,28 @@
 
-var buttonsColors=["red" , "blue" , "green" , "yellow"];
+var buttonsColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
-function nextSquence(){
+function nextSquence() {
     let n = Math.random();
-    n= n*4;
-    n=Math.floor(n);
-    var randomChosenColor= buttonsColors[n]; 
+    n = n * 4;
+    n = Math.floor(n);
+    var randomChosenColor = buttonsColors[n];
     gamePattern.push(randomChosenColor);
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
     audio.play();
 }
 
+$(".btn").on("click", function () {
+    var clickedButtonId = $(this).attr("id");
+    userClickedPattern.push(clickedButtonId);
+    playSounds(clickedButtonId);
+    console.log(clickedButtonId);
+});
+
+function playSounds(id){
+    $("#" + id).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + id + ".mp3");
+    audio.play();
+}
