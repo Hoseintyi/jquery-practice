7f7828660a5747bd7876b35b9b7afe21d7121d4c
Hoseintyi <hoseint629@gmail.com>
2023-11-20 04:21:11 +0330

    fisst level of game with any press

--- game.js
+++ game.js
@@ -2,8 +2,11 @@
 var buttonsColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-
+var level = 0;
+var started= false;
 function nextSquence() {
+    level++;
+    $("#level-title").text("Level " + level);
     let n = Math.random();
     n = n * 4;
     n = Math.floor(n);
@@ -17,12 +20,20 @@ function nextSquence() {
 $(".btn").on("click", function () {
     var clickedButtonId = $(this).attr("id");
     userClickedPattern.push(clickedButtonId);
-    playSounds(clickedButtonId);
+    playSoundsAndAnimate(clickedButtonId);
     console.log(clickedButtonId);
 });
 
-function playSounds(id){
+function playSoundsAndAnimate(id){
     $("#" + id).fadeIn(100).fadeOut(100).fadeIn(100);
     var audio = new Audio("sounds/" + id + ".mp3");
     audio.play();
 }
+
+$(document).keypress(function(){
+if(!started){
+  $("#level-title").text("level"+ level);  
+  nextSquence();
+  started=true;
+}
+});
